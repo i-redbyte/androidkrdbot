98aea0d20512f7c78e4be64782163c343340b38f
Ilya Sokolov <developer.sokolov@gmail.com>
2025-05-13 00:35:21 +0300

    @feat:base chatbot

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,25 @@
 plugins {
     kotlin("jvm") version "2.0.0"
+    application
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "su.redbyte"
-version = "1.0-SNAPSHOT"
-
-repositories {
-    mavenCentral()
-}
+version = "1.0"
 
 dependencies {
-    testImplementation(kotlin("test"))
+    implementation("io.github.kotlin-telegram-bot.kotlin-telegram-bot:telegram:6.3.0")
+    implementation("io.github.cdimascio:dotenv-kotlin:6.5.1")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
+
+application {
+    mainClass.set("MainKt")
+}
+
 kotlin {
     jvmToolchain(20)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,18 @@
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
-}
 rootProject.name = "androidkrdbot"
 
+pluginManagement {
+    repositories {
+        maven( "https://jitpack.io" )
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        maven( "https://jitpack.io" )
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
