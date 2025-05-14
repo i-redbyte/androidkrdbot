3fbf020bf23c9d40658a8ba8f84a6c88282ee661
Ilya Sokolov <developer.sokolov@gmail.com>
2025-05-14 18:49:21 +0300

    @feat: update questions

--- src/main/kotlin/su/redbyte/androidkrdbot/domain/factory/QuestionFactory.kt
+++ src/main/kotlin/su/redbyte/androidkrdbot/domain/factory/QuestionFactory.kt
@@ -4,8 +4,34 @@ import su.redbyte.androidkrdbot.domain.model.Question
 
 object QuestionFactory {
     private val questions = listOf(
-        Question("Сколько будет шесть плюс шесть?", listOf("12", "двенадцать")),
-        Question("Назови любой цвет радуги", listOf("красный", "оранжевый", "жёлтый", "зелёный", "голубой", "синий", "фиолетовый"))
+        Question(
+            "Назови любой цвет радуги.",
+            listOf(
+                "красный", "оранжевый", "жёлтый", "желтый", "зелёный", "зеленый",
+                "голубой", "синий", "фиолетовый",
+                "red", "orange", "yellow", "green", "blue", "indigo", "violet"
+            )
+        ),
+        Question(
+            "Назовите столицу СССР.",
+            listOf("москва", "moscow")
+        ),
+        Question(
+            "Сколько букв в слове «проверка»?",
+            listOf("8", "восемь", "eight")
+        ),
+        Question(
+            "Главный цвет флага СССР?",
+            listOf("красный", "red")
+        ),
+        Question(
+            "Какой язык программирования используется в Android-разработке?",
+            listOf("kotlin", "java", "котлин", "джава")
+        ),
+        Question(
+            "Сколько бит в одном байте?",
+            listOf("8", "восемь", "eight")
+        ),
     )
 
     fun randomQuestion(): Question = questions.random()
