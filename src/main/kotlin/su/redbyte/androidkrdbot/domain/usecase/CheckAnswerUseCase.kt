5f1b93770e728cd187687046df5b43f7176669d1
Ilya Sokolov <developer.sokolov@gmail.com>
2025-05-14 18:01:11 +0300

    @fix: lowercase for user answer

--- src/main/kotlin/su/redbyte/androidkrdbot/domain/usecase/CheckAnswerUseCase.kt
+++ src/main/kotlin/su/redbyte/androidkrdbot/domain/usecase/CheckAnswerUseCase.kt
@@ -2,13 +2,14 @@ package su.redbyte.androidkrdbot.domain.usecase
 
 import com.github.kotlintelegrambot.Bot
 import su.redbyte.androidkrdbot.data.repository.VerificationRepository
+import java.util.*
 
 class CheckAnswerUseCase(
     private val verificationRepo: VerificationRepository
 ) {
     operator fun invoke(userId: Long, answer: String, bot: Bot) {
         val verification = verificationRepo.get(userId) ?: return
-        if (verification.question.isCorrect(answer)) {
+        if (verification.question.isCorrect(answer.lowercase(Locale.getDefault()))) {
             bot.sendMessage(
                 verification.chatId,
                 "${verification.user.firstName} успешно прошёл проверку! Добро пожаловать."
