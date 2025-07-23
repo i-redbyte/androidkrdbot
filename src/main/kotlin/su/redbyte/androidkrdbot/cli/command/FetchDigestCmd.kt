7dd261fb7c8b1fa8d6d50b998ba062b146ebb48b
Ilya Sokolov <developer.sokolov@gmail.com>
2025-07-23 12:29:10 +0300

    @fix: end symbol for NO_NEW state

--- src/main/kotlin/su/redbyte/androidkrdbot/cli/command/FetchDigestCmd.kt
+++ src/main/kotlin/su/redbyte/androidkrdbot/cli/command/FetchDigestCmd.kt
@@ -1,14 +1,15 @@
 package su.redbyte.androidkrdbot.cli.command
 
 import su.redbyte.androidkrdbot.domain.usecase.FetchDigestUseCase
+import su.redbyte.androidkrdbot.domain.usecase.FetchDigestUseCase.Companion.NO_NEW
 
 class FetchDigestCmd(
     private val fetchDigest: FetchDigestUseCase
 ) : BotCommand {
     override val name: String = Commands.DIGEST.commandName
     override suspend fun handle(ctx: CommandContext) {
-        val text = fetchDigest()
-        if (text.isEmpty()) {
+        val text = fetchDigest().trim()
+        if (text.isEmpty() || text == NO_NEW) {
             ctx.reply("В нашей агентурной сети пока нет новой информации. Продолжаем вести наблюдение \uD83D\uDC40")
             return
         }
--- src/main/kotlin/su/redbyte/androidkrdbot/domain/usecase/FetchDigestUseCase.kt
+++ src/main/kotlin/su/redbyte/androidkrdbot/domain/usecase/FetchDigestUseCase.kt
@@ -8,12 +8,10 @@ class FetchDigestUseCase(
     suspend operator fun invoke(): String {
         val result = repo.getDigest()
         if (result.isFailure) return "\uD83D\uDC80Наша агентурная сеть раскрыта, явки провалены, информация не добыта."
-        val text = result.getOrNull()
-        if (text == null || text == NO_NEW) return ""
-        return text
+        return result.getOrNull() ?: ""
     }
 
     companion object {
-        private const val NO_NEW = "NO_NEW" //convert to ENUM
+        const val NO_NEW = "NO_NEW" //convert to ENUM
     }
 }
