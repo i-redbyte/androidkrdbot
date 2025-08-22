ce8e80d1c3c5da4bd7bed1b3419b73c4cac73d1f
Ilya Sokolov <developer.sokolov@gmail.com>
2025-08-22 03:15:59 +0300

    @feat: update game

--- src/main/kotlin/su/redbyte/androidkrdbot/cli/command/GameCmd.kt
+++ src/main/kotlin/su/redbyte/androidkrdbot/cli/command/GameCmd.kt
@@ -2,26 +2,22 @@ package su.redbyte.androidkrdbot.cli.command
 
 import com.github.kotlintelegrambot.entities.InlineKeyboardMarkup
 import com.github.kotlintelegrambot.entities.keyboard.InlineKeyboardButton
-import com.github.kotlintelegrambot.entities.keyboard.WebAppInfo
 
 class GameCmd : BotCommand {
     override val name: String = Commands.GAME.commandName
 
     override suspend fun handle(ctx: CommandContext) {
-        val rows = listOf(
-            listOf(
-                InlineKeyboardButton.WebApp(
-                    text = "\uD83D\uDD79\uFE0F Играть",
-                    webApp = WebAppInfo(url = "https://game.red-byte.ru")
-                )
-            )
-        )
+        val botUsername = "androidkrd_beria_bot"
+        val shortName = "android_krd_game"
+        val deepLink = "https://t.me/$botUsername/$shortName?startapp=from_group"
 
-        val kb = InlineKeyboardMarkup.create(rows)
+        val kb = InlineKeyboardMarkup.create(
+            listOf(listOf(InlineKeyboardButton.Url("🎮 Играть", deepLink)))
+        )
 
         ctx.bot.sendMessage(
             chatId = ctx.chatId,
-            text = "Поработал, отдохни!",
+            text = "Поработал — отдохни!",
             replyMarkup = kb,
             messageThreadId = ctx.message.messageThreadId
         )
